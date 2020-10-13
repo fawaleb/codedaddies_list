49e4cfd7b921af8fa04e69fe602f86c01932a5e9
Peter Landy <peter.landy@r9gl.com>
2020-10-13 16:29:01 +0200

    Updated base HTML new_search.html views and URLS

--- my_app/views.py
+++ my_app/views.py
@@ -7,4 +7,9 @@ def home(request):
 
 
 def new_search(request):
-    return render(request,'my_app/new_search.html')
+    search = request.POST.get('search')
+    print(search)
+    stuff_for_frontend={
+        'search':search,
+    }
+    return render(request,'my_app/new_search.html',stuff_for_frontend)
