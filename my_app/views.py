ba7d87b7433eaf7495a539c6c17bb95d6de767a7
Peter Landy <peter.landy@r9gl.com>
2020-10-15 09:23:28 +0200

    Final Front End Layout

--- my_app/views.py
+++ my_app/views.py
@@ -7,9 +7,7 @@ from requests import compat
 import requests
 
 BASE_URL = 'https://losangeles.craigslist.org/search/?query={}'
-
-
-
+BASE_IMAGE_URL='https://images.craigslist.org/{}_300x300.jpg'
 
 
 # Create your views here.
@@ -22,25 +20,30 @@ def new_search(request):
     models.Search.objects.create(search=search)
     final_url = BASE_URL.format(quote_plus(search))
     response = requests.get(final_url)
-    data=response.text
-    soup = BeautifulSoup(data,features='html.parser')
-    post_listings=soup.find_all('li',{'class':'result-row'})
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_listings = soup.find_all('li', {'class': 'result-row'})
 
-
-
-    final_posting=[]
+    final_posting = []
     for post in post_listings:
-        post_title=post.find(class_='result-title').text
+        post_title = post.find(class_='result-title').text
         post_url = post.find('a').get('href')
         if post.find(class_='result-price'):
             post_price = post.find(class_='result-price').text
         else:
-            post_price='N/A'
-        final_posting.append((post_title,post_url,post_price))
-        
+            post_price = 'N/A'
+
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id=post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url=BASE_IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url="https://craigslist.org/images/peace.jpg"
+        final_posting.append((post_title, post_url, post_price,post_image_url))
+
 
     stuff_for_frontend = {
         'search': search,
-        'final_postings':final_posting,
+        'final_postings': final_posting,
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
